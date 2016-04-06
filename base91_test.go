9911d3d6c2aab9a3532b1dd94b95210b48676a99
Jeff Becker <ampernand@gmail.com>
2016-04-06 07:57:15 -0400

    gofmt and add Encoder / Decoder

--- base91_test.go
+++ base91_test.go
@@ -1,23 +1,30 @@
-
-
 package base91
 
-
 import (
-  "testing"
-  "bytes"
-  "crypto/rand"
-  "io"
+	"bytes"
+	"crypto/rand"
+	"io"
+	"testing"
 )
 
 func TestBase91(t *testing.T) {
-  var buff [10]byte
-  io.ReadFull(rand.Reader, buff[:])
-  out := Encode(buff[:])
-  t.Logf("%q -> %q",buff[:], out)
-  buff2 := Decode(out)
-  if ! bytes.Equal(buff2, buff[:]) {
-    t.Logf("%q vs %q", buff2, buff)
-    t.Fail()
-  }
+	n := 0
+	for n < 1028 {
+		buff := make([]byte, n)
+		io.ReadFull(rand.Reader, buff[:])
+		out := Encode(buff[:])
+		t.Logf("%q -> %q", buff[:], out)
+		buff2, err := Decode(out)
+		if err == nil {
+			if !bytes.Equal(buff2, buff[:]) {
+				t.Logf("%q vs %q", buff2, buff)
+				t.Fail()
+			}
+		} else {
+			t.Logf("error: %s", err.Error())
+			t.Fail()
+		}
+		n++
+		buff = nil
+	}
 }
